5cd6cd975982c611c095adc7c2cab9a01c4b9e31
P. Chen <pengyu@libstarrify.so>
2020-12-15 08:41:50 +0000

    Added: 2020 - D15P1

---
+++ 2020/day_15_part_1.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+awk -F, '
+    {
+        for (i = 1; i <= NF; i++) {
+            last_seen[$i] = i;
+            prev = $i;
+        }
+    }
+    END {
+        for (i = NF + 1; i <= 2020; i++) {
+            if (prev in last_seen) {
+                age = i - last_seen[prev] - 1;
+            } else {
+                age = 0;
+            }
+            last_seen[prev] = i - 1;
+            prev = age;
+        }
+        print(prev);
+    }
+'
