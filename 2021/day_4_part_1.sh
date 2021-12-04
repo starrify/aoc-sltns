63adaf86fd3785c3711d3c888459dbdf44378819
Peng-Yu Chen <pengyu@libstarrify.so>
2021-12-04 05:25:01 +0000

    2021 - day 4 part 1

---
+++ 2021/day_4_part_1.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+awk '
+NR == 1 {
+    split($0, seq, ",");
+}
+NR > 1 && NR % 6 != 2 {
+    nboard = int((NR + 3) / 6);
+    row = (NR + 4) % 6;
+    for (i = 1; i <= 5; i++) {
+        boards[nboard, row, i] = $i;
+        board_sum[nboard] += $i;
+    }
+}
+END {
+    for (taken in seq) {
+        for (board = 1; board <= nboard; board++) {
+            for (i = 1; i <= 5; i++)
+                for (j = 1; j <= 5; j++)
+                    if (boards[board, i, j] == seq[taken]) {
+                        board_sum[board] -= seq[taken];
+                        board_matched_x[board, i] += 1;
+                        if (board_matched_x[board, i] == 5) {
+                            print(board_sum[board] * seq[taken]);
+                            exit;
+                        }
+                        board_matched_y[board, j] += 1;
+                        if (board_matched_y[board, j] == 5) {
+                            print(board_sum[board] * seq[taken]);
+                            exit;
+                        }
+                    }
+        }
+    }
+}
+'
